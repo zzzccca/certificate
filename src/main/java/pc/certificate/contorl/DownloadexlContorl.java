a653cd2c70a01c95a154b912fbe0243892d2eb0c
zzzccca <a820714>
2017-08-31 09:03:44 +0800

    代码上传  添加导出待寄

--- src/main/java/pc/certificate/contorl/DownloadexlContorl.java
+++ src/main/java/pc/certificate/contorl/DownloadexlContorl.java
@@ -1,6 +1,5 @@
 package pc.certificate.contorl;
 
-import org.apache.poi.hssf.usermodel.*;
 import org.apache.poi.ss.usermodel.HorizontalAlignment;
 import org.apache.poi.xssf.usermodel.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import pc.certificate.domain.Expressage;
 import pc.certificate.domain.User;
+import pc.certificate.reop.ExpressageRepository;
 import pc.certificate.reop.UserRepository;
 import pc.certificate.service.DesService;
 
@@ -25,6 +26,9 @@ public class DownloadexlContorl {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private ExpressageRepository expressageRepository;
 
     @Autowired
     private DesService desService;
@@ -103,4 +107,83 @@ public class DownloadexlContorl {
         }
         workbook.write(res.getOutputStream());
     }
+
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    @RequestMapping("/getexpressageexl")
+    public void getexpressageexl(HttpServletResponse res) throws IOException
+    {
+
+        res.setContentType("application/octet-stream");
+        res.setHeader("Content-Disposition", "attachment;filename=Expressage.xlsx" );//下载后的word名
+        res.flushBuffer();
+
+        XSSFWorkbook workbook = new XSSFWorkbook();
+        XSSFSheet sheet = workbook.createSheet("统计表");
+        createTitle2(workbook, sheet);
+        String type="待寄";
+        List<Expressage> entities = this.expressageRepository.findByType(type);
+
+        //新增数据行，并且设置单元格数据
+        int rowNum = 1;
+        for (Expressage expressage:entities) {
+
+            XSSFRow row = sheet.createRow(rowNum);
+            row.createCell(0).setCellValue(expressage.getName());
+            row.createCell(1).setCellValue(expressage.getCertificatename());
+            row.createCell(2).setCellValue(expressage.getCertificatenumber());
+            row.createCell(3).setCellValue(expressage.getPhone());
+            row.createCell(4).setCellValue(expressage.getAddress());
+            row.createCell(5).setCellValue(expressage.getOddnumber());
+            row.createCell(6).setCellValue(expressage.getType());
+            XSSFCell cell = row.createCell(7);
+            rowNum++;
+        }
+        workbook.write(res.getOutputStream());
     }
+
+    /***
+     * 创建表头
+     * @param workbook
+     * @param sheet
+     */
+    private void createTitle2(XSSFWorkbook workbook, XSSFSheet sheet)
+    {
+        XSSFRow row = sheet.createRow(0);
+
+        //设置为居中加粗
+        XSSFCellStyle style = workbook.createCellStyle();
+        XSSFFont font = workbook.createFont();
+        font.setBold(true);
+        style.setAlignment(HorizontalAlignment.CENTER);
+        style.setFont(font);
+
+        XSSFCell cell;
+        cell = row.createCell(0);
+        cell.setCellValue("姓名");
+        cell.setCellStyle(style);
+
+        cell = row.createCell(1);
+        cell.setCellValue("证书名称");
+        cell.setCellStyle(style);
+
+        cell = row.createCell(2);
+        cell.setCellValue("证书编号");
+        cell.setCellStyle(style);
+
+        cell = row.createCell(3);
+        cell.setCellValue("手机");
+        cell.setCellStyle(style);
+
+        cell = row.createCell(4);
+        cell.setCellValue("寄件地址");
+        cell.setCellStyle(style);
+
+        cell = row.createCell(5);
+        cell.setCellValue("快递单号");
+        cell.setCellStyle(style);
+
+        cell = row.createCell(6);
+        cell.setCellValue("寄件状态");
+        cell.setCellStyle(style);
+    }
+}
