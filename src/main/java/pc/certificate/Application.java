68fb45e9e5cc4b76878abc1d433f462501cff567
zzzccca <a820714>
2017-09-04 10:32:36 +0800

    代码上传  定时（每天0点）查询ems订单状态是否已经签收，更改平台状态为已收

--- src/main/java/pc/certificate/Application.java
+++ src/main/java/pc/certificate/Application.java
@@ -1,17 +1,15 @@
 package pc.certificate;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * Created by wu on 17-6-13.
  */
-@EnableAutoConfiguration
-@ComponentScan
+@EnableScheduling
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
