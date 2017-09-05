a58e84d9704a7a611b24788bea58ec5fd2da5698
liutim <84539606@qq.com>
2017-09-05 13:49:44 +0800

    使用spring session生成x-auth-token

---
+++ src/main/java/pc/certificate/Initializer.java
@@ -0,0 +1,10 @@
+package pc.certificate;
+
+import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
+
+/**
+ * Created by liutim on 2017/9/5.
+ */
+public class Initializer extends AbstractHttpSessionApplicationInitializer {
+
+}
